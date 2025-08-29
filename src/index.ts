005f7abea7f71d55c90657a229fed7d00fccd849
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-08-29 16:09:10 +0400

    chore: add Redis connection to the server initialization

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import http from "http";
 import express from "express";
 
 import connectMongoDB from "./config/mongoDB.js";
+import { connectRedis } from "./config/redis.js";
 
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
@@ -12,6 +13,7 @@ const app = express();
 const server = http.createServer(app);
 
 connectMongoDB(process.env.MONGO_URI as string);
+connectRedis(process.env.REDIS_CLI as string);
 
 const port = Number(process.env.PORT) || 5000;
 
