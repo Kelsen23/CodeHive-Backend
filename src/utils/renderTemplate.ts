a26cb005af16f914408fde23739e054ed948ebf7
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-08-31 14:36:27 +0400

    feat(email-templates): add verification email template and rendering function

---
+++ src/utils/renderTemplate.ts
@@ -0,0 +1,35 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import mjml2html from "mjml";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function renderTemplate(
+  templateName: string,
+  variables: Record<string, string>,
+) {
+  const filePath = path.join(__dirname, "../emails", `${templateName}.mjml`);
+  const mjml = fs.readFileSync(filePath, "utf-8");
+  const { html } = mjml2html(mjml);
+
+  return html.replace(/\{\{(\w+)\}\}/g, (_, key) => variables[key] || "");
+}
+
+const verificationHtml = (
+  username: string,
+  otp: string,
+  deviceName: string,
+  deviceIp: string,
+) => {
+  return renderTemplate("verification", {
+    username,
+    otp,
+    deviceName,
+    ip: deviceIp,
+    year: new Date().getFullYear().toString(),
+  });
+};
+
+export { verificationHtml };
