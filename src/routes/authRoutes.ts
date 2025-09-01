bcf754222f3e869252b6865717a41acc5e13f4a9
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-01 12:29:55 +0400

    feat(authRoutes): implement route for registerOrLogin controller with Zod validation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,22 @@
 import express from "express";
 
-import { login, register } from "../controllers/authController.js";
+import {
+  login,
+  register,
+  registerOrLogin,
+} from "../controllers/authController.js";
 
 import validate from "../middlewares/validateMiddleware.js";
-import { loginSchema, registerSchema } from "../validations/auth.schema.js";
+import {
+  loginSchema,
+  oauthSchema,
+  registerSchema,
+} from "../validations/auth.schema.js";
 
 const router = express.Router();
 
 router.route("/register").post(validate(registerSchema), register);
-router.route("/login").post(validate(loginSchema), login);  
+router.route("/login").post(validate(loginSchema), login);
+router.route("/registerOrLogin").post(validate(oauthSchema), registerOrLogin);
 
 export default router;
