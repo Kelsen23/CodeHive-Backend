8d1abf302109e34a2a7efd3521cd8b97b643096b
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-01 13:24:26 +0400

    feat(authRoutes): add route for email verification

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,13 +4,16 @@ import {
   login,
   register,
   registerOrLogin,
+  verifyEmail,
 } from "../controllers/authController.js";
 
+import isAuthenticated from "../middlewares/authMiddleware.js";
 import validate from "../middlewares/validateMiddleware.js";
 import {
   loginSchema,
   oauthSchema,
   registerSchema,
+  verifyEmailSchema,
 } from "../validations/auth.schema.js";
 
 const router = express.Router();
@@ -18,5 +21,8 @@ const router = express.Router();
 router.route("/register").post(validate(registerSchema), register);
 router.route("/login").post(validate(loginSchema), login);
 router.route("/registerOrLogin").post(validate(oauthSchema), registerOrLogin);
+router
+  .route("/verifyEmail")
+  .post(isAuthenticated, validate(verifyEmailSchema), verifyEmail);
 
 export default router;
