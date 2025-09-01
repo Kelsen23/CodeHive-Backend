d06ba376da163357bc990f9f994683609a81b22e
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-01 11:01:51 +0400

    feat(authRoutes): register /login route for authRouter with zod schema validation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+
+import { login, register } from "../controllers/authController.js";
+
 import validate from "../middlewares/validateMiddleware.js";
-import { registerSchema } from "../validations/auth.schema.js";
+import { loginSchema, registerSchema } from "../validations/auth.schema.js";
 
 const router = express.Router();
 
 router.route("/register").post(validate(registerSchema), register);
+router.route("/login").post(validate(loginSchema), login);
 
 export default router;
