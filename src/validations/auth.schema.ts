6c34e8fef7455224e52bfaf75dad8fa6175684ed
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-01 12:30:58 +0400

    feat(auth.schema): create create googleSchema and githubSchema for validating Google and Github provider's sent bodies

--- src/validations/auth.schema.ts
+++ src/validations/auth.schema.ts
@@ -28,4 +28,37 @@ const loginSchema = z.object({
     .max(60, "Password must be at most 60 characters"),
 });
 
+const googleSchema = z.object({
+  provider: z.literal("google"),
+  email: z
+    .string()
+    .email("Invalid email address")
+    .min(5, "Email must be at least 5 characters")
+    .max(345, "Email must be at most 345 characters"),
+  name: z
+    .string()
+    .min(3, "Username must be atleast 3 characters")
+    .max(15, "Username must be at most 15 characters"),
+  picture: z.string().url("Invalid picture url"),
+  email_verified: z.boolean(),
+});
+
+const githubSchema = z.object({
+  provider: z.literal("github"),
+  email: z
+    .string()
+    .email("Invalid email address")
+    .min(5, "Email must be at least 5 characters")
+    .max(345, "Email must be at most 345 characters"),
+  name: z
+    .string()
+    .min(3, "Username must be atleast 3 characters")
+    .max(15, "Username must be at most 15 characters"),
+  avatar_url: z.string().url("Invalid picture url"),
+});
+
 export { registerSchema, loginSchema };
+export const oauthSchema = z.discriminatedUnion("provider", [
+  googleSchema,
+  githubSchema,
+]);
