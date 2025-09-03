e71b8754f39886584beeac87046ea0b475d0da40
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-03 10:08:04 +0400

    refactor(authRoutes): refactor authRoutes code

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -22,6 +22,7 @@ const router = express.Router();
 router.route("/register").post(validate(registerSchema), register);
 router.route("/login").post(validate(loginSchema), login);
 router.route("/registerOrLogin").post(validate(oauthSchema), registerOrLogin);
+
 router
   .route("/verifyEmail")
   .post(isAuthenticated, validate(verifyEmailSchema), verifyEmail);
