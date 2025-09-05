4247b5c470fb70581a1bc19772d70d8e8ff99117
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-05 18:35:06 +0400

    fix(index): fix the name of imported authRoutes

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import DataLoader from "dataloader";
 import typeDefs from "./graphql/typeDefs/index.js";
 import resolvers from "./graphql/resolvers/index.js";
 
-import authRouter from "./routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import uploadFileRoutes from "./routes/uploadFileRoutes.js";
 
 import cookieParser from "cookie-parser";
@@ -48,7 +48,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRoutes);
 app.use("/api/upload", uploadFileRoutes);
 
 app.use(
