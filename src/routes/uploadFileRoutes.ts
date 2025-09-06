9b40c98ade722295968a35b770bfa3f9e007c085
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-06 16:31:45 +0400

    feat(uploadFileRoutes): create uploadFileRoutes and create a route for changing profile picture

--- src/routes/uploadFileRoutes.ts
+++ src/routes/uploadFileRoutes.ts
@@ -1,10 +1,24 @@
 import express from "express";
 
 import multer from "multer";
+import { changeProfilePicture } from "../controllers/uploadFileController.js";
+
+import isAuthenticated from "../middlewares/authMiddleware.js";
+
+import { uploadProfilePictureLimiterMiddleware } from "../middlewares/rateLimiters/uploadFileLimiters.js";
 
 const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
+
+router
+  .route("/profilePicture")
+  .post(
+    uploadProfilePictureLimiterMiddleware,
+    isAuthenticated,
+    upload.single("profilePicture"),
+    changeProfilePicture,
+  );
 
 export default router;
