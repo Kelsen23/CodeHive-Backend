df39ac7ac9447db734a1913c9985c10616be3fa9
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-20 11:21:49 +0400

    feat(userController): add getInterests and saveInterests functions

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -6,6 +6,7 @@ import { prisma } from "../index.js";
 import { redisClient } from "../config/redis.js";
 
 import HttpError from "../utils/httpError.js";
+import interests from "../utils/interests.js";
 
 interface AuthenticatedRequest extends Request {
   cookies: {
@@ -55,13 +56,31 @@ const updateProfile = asyncHandler(
       60 * 60,
     );
 
-    return res
-      .status(200)
-      .json({
-        message: "Successfully updated profile",
-        user: userWithoutSensitiveInfo,
-      });
+    return res.status(200).json({
+      message: "Successfully updated profile",
+      user: userWithoutSensitiveInfo,
+    });
   },
 );
 
-export { updateProfile };
+const getInterests = asyncHandler(async (req: Request, res: Response) => {
+  return res.status(200).json({ interests });
+});
+
+const saveInterests = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user;
+    const { interests } = req.body;
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: { interests },
+    });
+
+    return res.status(200).json({
+      message: "Successfully saved interests",
+      interests: updatedUser.interests,
+    });
+  },
+);
+export { updateProfile, getInterests, saveInterests };
