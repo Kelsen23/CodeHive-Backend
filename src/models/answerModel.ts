b96f80024e11e577827d1260296e6aa0c97cc2b5
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-20 13:51:20 +0400

    feat(answerModel): create Answer schema for managing answers in the database

---
+++ src/models/answerModel.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const AnswerSchema: Schema = new Schema(
+  {
+    questionId: {
+      type: Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+    },
+    userId: { type: String, required: true },
+    body: { type: String, required: true, maxlength: 5000 },
+    isBestAnswer: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Answer", AnswerSchema);
