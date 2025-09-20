fd0aaa22a64bee9850bdc773a35a1114ddf07d15
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-09-20 12:10:37 +0400

    feat(userRoutes): add routes for getting and saving interests with rate limiting

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,6 +1,10 @@
 import express from "express";
 
-import { updateProfile } from "../controllers/userController.js";
+import {
+  getInterests,
+  saveInterests,
+  updateProfile,
+} from "../controllers/userController.js";
 
 import isAuthenticated, {
   isTerminated,
@@ -11,6 +15,8 @@ import validate from "../middlewares/validateMiddleware.js";
 import { updateProfileSchema } from "../validations/user.schema.js";
 
 import { updateProfileLimiterMiddleware } from "../middlewares/rateLimiters/userRateLimiters.js";
+import { getInterestsLimiterMiddleware } from "../middlewares/rateLimiters/userRateLimiters.js";
+import { saveInterestsLimiterMiddleware } from "../middlewares/rateLimiters/userRateLimiters.js";
 
 const router = express.Router();
 
@@ -23,6 +29,25 @@ router
     isTerminated,
     validate(updateProfileSchema),
     updateProfile,
+  );
+
+router
+  .route("/getInterests")
+  .get(
+    getInterestsLimiterMiddleware,
+    isAuthenticated,
+    isVerified,
+    isTerminated,
+    getInterests,
+  );
+router
+  .route("/saveInterests")
+  .put(
+    saveInterestsLimiterMiddleware,
+    isAuthenticated,
+    isVerified,
+    isTerminated,
+    saveInterests,
   );
 
 export default router;
