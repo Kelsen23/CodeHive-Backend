da60c547633417aec78648e3f7a64f835034a891
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-10-04 15:56:21 +0400

    feat(graphql): include question resolvers in the merged resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,6 +1,8 @@
 import { mergeResolvers } from "@graphql-tools/merge";
-import userResolvers from "./userResolvers.js";
 
-const resolvers = mergeResolvers([userResolvers]);
+import userResolvers from "./userResolvers.js";
+import questionResolvers from "./questionResolvers.js";
+
+const resolvers = mergeResolvers([userResolvers, questionResolvers]);
 
 export default resolvers;
