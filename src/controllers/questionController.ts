97b04debf09d05acc5655b1022dc34b0519c475c
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-10-11 18:17:30 +0400

    feat(questionController): add createAnswerOnQuestion resolver for answer creation

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -5,6 +5,9 @@ import { Request, Response } from "express";
 import AuthenticatedRequest from "../types/authenticatedRequest.js";
 
 import Question from "../models/questionModel.js";
+import Answer from "../models/answerModel.js";
+
+import HttpError from "../utils/httpError.js";
 
 const createQuestion = asyncHandler(
   async (req: AuthenticatedRequest, res: Response) => {
@@ -25,4 +28,25 @@ const createQuestion = asyncHandler(
   },
 );
 
-export { createQuestion };
+const createAnswerOnQuestion = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user;
+    const { body } = req.body;
+    const { questionId } = req.params;
+
+    const foundQuestion = await Question.findById(questionId);
+
+    if (!foundQuestion) throw new HttpError("Question not found", 404);
+
+    if (foundQuestion.isDeleted || !foundQuestion.isActive)
+      throw new HttpError("Question not active", 404);
+
+    const newAnswer = await Answer.create({ questionId, body, userId });
+
+    return res
+      .status(201)
+      .json({ message: "Successfully created answer", answer: newAnswer });
+  },
+);
+
+export { createQuestion, createAnswerOnQuestion };
