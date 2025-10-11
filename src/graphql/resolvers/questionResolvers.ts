226d29d09ee000cb1164efd228601429b026b616
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-10-11 15:00:39 +0400

    feat(questionResolvers): implement getQuestionById resolver with caching and user data enrichment

--- src/graphql/resolvers/questionResolvers.ts
+++ src/graphql/resolvers/questionResolvers.ts
@@ -5,6 +5,8 @@ import Vote from "../../models/voteModel.js";
 
 import UserWithoutSensitiveInfo from "../../types/userWithoutSesitiveInfo.js";
 
+import mongoose from "mongoose";
+
 const questionResolvers = {
   Query: {
     getRecommendedQuestions: async (
@@ -121,6 +123,221 @@ const questionResolvers = {
       );
 
       return questionsWithUsers;
+    },
+
+    getQuestionById: async (
+      _: any,
+      { id }: { id: string },
+      {
+        user,
+        redisClient,
+        loaders,
+      }: { user: UserWithoutSensitiveInfo; redisClient: any; loaders: any },
+    ) => {
+      const cachedQuestion = await redisClient.get(`question:${id}`);
+      if (cachedQuestion) return JSON.parse(cachedQuestion);
+
+      const questionData = await Question.aggregate([
+        { $match: { _id: new mongoose.Types.ObjectId(id) } },
+
+        {
+          $lookup: {
+            from: "answers",
+            localField: "_id",
+            foreignField: "questionId",
+            as: "answers",
+          },
+        },
+
+        { $addFields: { answerCount: { $size: "$answers" } } },
+
+        {
+          $lookup: {
+            from: "votes",
+            localField: "answers._id",
+            foreignField: "targetId",
+            as: "answerVotes",
+          },
+        },
+
+        {
+          $addFields: {
+            answers: {
+              $map: {
+                input: "$answers",
+                as: "ans",
+                in: {
+                  $mergeObjects: [
+                    "$$ans",
+                    {
+                      upvotes: {
+                        $size: {
+                          $filter: {
+                            input: "$answerVotes",
+                            as: "v",
+                            cond: {
+                              $and: [
+                                { $eq: ["$$v.targetId", "$$ans._id"] },
+                                { $eq: ["$$v.voteType", "upvote"] },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                      downvotes: {
+                        $size: {
+                          $filter: {
+                            input: "$answerVotes",
+                            as: "v",
+                            cond: {
+                              $and: [
+                                { $eq: ["$$v.targetId", "$$ans._id"] },
+                                { $eq: ["$$v.voteType", "downvote"] },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+
+        {
+          $addFields: {
+            topAnswer: {
+              $let: {
+                vars: {
+                  filteredTop: {
+                    $filter: {
+                      input: "$answers",
+                      as: "a",
+                      cond: { $eq: ["$$a.isTopAnswer", true] },
+                    },
+                  },
+                },
+                in: {
+                  $cond: {
+                    if: { $gt: [{ $size: "$$filteredTop" }, 0] },
+                    then: { $first: "$$filteredTop" },
+                    else: {
+                      $first: {
+                        $sortArray: {
+                          input: "$answers",
+                          sortBy: { upvotes: -1, createdAt: 1 },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+
+        {
+          $lookup: {
+            from: "replys",
+            let: { topAnswerId: "$topAnswer._id" },
+            pipeline: [
+              { $match: { $expr: { $eq: ["$answerId", "$$topAnswerId"] } } },
+              {
+                $lookup: {
+                  from: "votes",
+                  localField: "_id",
+                  foreignField: "targetId",
+                  as: "votes",
+                },
+              },
+              {
+                $addFields: {
+                  upvotes: {
+                    $size: {
+                      $filter: {
+                        input: "$votes",
+                        as: "v",
+                        cond: { $eq: ["$$v.voteType", "upvote"] },
+                      },
+                    },
+                  },
+                  downvotes: {
+                    $size: {
+                      $filter: {
+                        input: "$votes",
+                        as: "v",
+                        cond: { $eq: ["$$v.voteType", "downvote"] },
+                      },
+                    },
+                  },
+                },
+              },
+              { $sort: { upvotes: -1, createdAt: 1 } },
+              { $limit: 1 },
+            ],
+            as: "topAnswer.replies",
+          },
+        },
+
+        {
+          $project: {
+            _id: 1,
+            title: 1,
+            body: 1,
+            tags: 1,
+            upvotes: 1,
+            downvotes: 1,
+            isDeleted: 1,
+            isActive: 1,
+            createdAt: 1,
+            topAnswer: 1,
+            answers: 1,
+            answerCount: 1,
+            askerId: 1,
+          },
+        },
+      ]);
+
+      const question = questionData[0];
+
+      const questionUserIds = [question.askerId];
+      const answerUserIds = question.answers.map((a: any) => a.userId);
+      const replyUserIds =
+        question.topAnswer?.replies.map((r: any) => r.userId) || [];
+
+      const allUserIds = [
+        ...new Set([...questionUserIds, ...answerUserIds, ...replyUserIds]),
+      ];
+
+      const users = await loaders.userLoader.loadMany(allUserIds);
+      const userMap = new Map();
+      users.forEach((u: any) => userMap.set(u.id.toString(), u));
+
+      question.user = userMap.get(question.askerId.toString());
+      question.answers = question.answers.map((a: any) => ({
+        ...a,
+        user: userMap.get(a.userId.toString()),
+      }));
+
+      if (question.topAnswer) {
+        question.topAnswer.replies = question.topAnswer.replies.map(
+          (r: any) => ({
+            ...r,
+            user: userMap.get(r.userId.toString()),
+          }),
+        );
+      }
+
+      await redisClient.set(
+        `question:${id}`,
+        JSON.stringify(question),
+        "EX",
+        60 * 15,
+      );
+
+      return question;
     },
   },
 };
