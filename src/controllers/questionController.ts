4ed6b76977f4d7cd1ae6f3ed00811614bcfa36c7
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-10-12 15:51:59 +0400

    feat(questionController): add createReplyOnAnswer function and update redis cache handling

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -8,6 +8,9 @@ import Question from "../models/questionModel.js";
 import Answer from "../models/answerModel.js";
 
 import HttpError from "../utils/httpError.js";
+import Reply from "../models/replyModel.js";
+
+import { redisClient } from "../config/redis.js";
 
 const createQuestion = asyncHandler(
   async (req: AuthenticatedRequest, res: Response) => {
@@ -39,9 +42,11 @@ const createAnswerOnQuestion = asyncHandler(
     if (!foundQuestion) throw new HttpError("Question not found", 404);
 
     if (foundQuestion.isDeleted || !foundQuestion.isActive)
-      throw new HttpError("Question not active", 404);
+      throw new HttpError("Question not active", 410);
 
     const newAnswer = await Answer.create({ questionId, body, userId });
+
+    await redisClient.del(`question:${questionId}`);
 
     return res
       .status(201)
@@ -49,4 +54,34 @@ const createAnswerOnQuestion = asyncHandler(
   },
 );
 
-export { createQuestion, createAnswerOnQuestion };
+const createReplyOnAnswer = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user;
+    const { body } = req.body;
+    const answerId = req.params.answerId;
+
+    const foundAnswer = await Answer.findById(answerId);
+
+    if (!foundAnswer) throw new HttpError("Answer not found", 404);
+
+    if (foundAnswer.isDeleted || !foundAnswer.isActive)
+      throw new HttpError("Answer not active", 410);
+
+    const foundQuestion = await Question.findById(foundAnswer.questionId);
+
+    if (!foundQuestion) throw new HttpError("Question not found", 404);
+
+    if (foundQuestion.isDeleted || !foundQuestion.isActive)
+      throw new HttpError("Question not active", 410);
+
+    const newReply = await Reply.create({ answerId, userId, body });
+
+    await redisClient.del(`question:${foundAnswer.questionId}`);
+
+    return res
+      .status(201)
+      .json({ message: "Successfully created reply", reply: newReply });
+  },
+);
+
+export { createQuestion, createAnswerOnQuestion, createReplyOnAnswer };
