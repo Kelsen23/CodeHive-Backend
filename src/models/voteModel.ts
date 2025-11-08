4d365540a1882d36e1080ba2a34749263ea27891
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-11-08 12:19:37 +0400

    refactor(questionModel): add toJSON for VoteSchema to delete versionKey, _id fields and add id field when mongoose objects are converted to JSON

--- src/models/voteModel.ts
+++ src/models/voteModel.ts
@@ -3,11 +3,26 @@ import mongoose, { Schema } from "mongoose";
 const VoteSchema = new Schema(
   {
     userId: { type: String, required: true },
-    targetType: { type: String, enum: ["Question", "Answer", "Reply"], required: true },
+    targetType: {
+      type: String,
+      enum: ["Question", "Answer", "Reply"],
+      required: true,
+    },
     targetId: { type: Schema.Types.ObjectId, required: true },
     voteType: { type: String, enum: ["upvote", "downvote"], required: true },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: (_, ret: any) => {
+        ret.id = ret._id;
+        delete ret._id;
+        return ret;
+      },
+    },
+  },
 );
 
 export default mongoose.model("Vote", VoteSchema);
