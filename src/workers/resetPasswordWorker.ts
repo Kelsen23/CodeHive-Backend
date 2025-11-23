ca2b62b2fbdba3fb2af46d9943b3d5a655a08ab9
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-11-23 22:38:32 +0400

    feat(queue): implement resetPasswordWorker using BullMQ for handling password reset requests

---
+++ src/workers/resetPasswordWorker.ts
@@ -0,0 +1,19 @@
+import { Worker } from "bullmq";
+import { redisConnection } from "../config/redis.js";
+import transporter from "../config/nodemailer.js";
+
+new Worker(
+  "resetPasswordQueue",
+  async (job) => {
+    const { email, htmlContent } = job.data;
+
+    await transporter.sendMail({
+      from: `'CodeHive' <${process.env.CODEHIVE_EMAIL}>`,
+      to: email,
+      subject: "Reset Password Request",
+      html: htmlContent,
+    });
+  },
+
+  { connection: redisConnection, concurrency: 20 },
+);
