c72a0e68dee67cf40a335313f79ca575766d8996
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2025-11-29 12:27:09 +0400

    feat(report): create report model with schema for handling reports

---
+++ src/models/reportModel.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const ReportSchema = new mongoose.Schema(
+  {
+    reportedBy: { type: String, required: true },
+    targetId: { type: String, required: true },
+    targetType: {
+      type: String,
+      required: true,
+      enum: ["User", "Question", "Answer", "Reply"],
+    },
+    targetUserId: { type: String, required: true },
+
+    reportReason: {
+      type: String,
+      required: true,
+      enum: [
+        "SPAM",
+        "HARASSMENT",
+        "HATE_SPEECH",
+        "INAPPROPRIATE_CONTENT",
+        "MISINFORMATION",
+        "OTHER",
+      ],
+    },
+    reportComment: {
+      type: String,
+      maxlength: 150,
+      minlength: 3,
+      default: null,
+    },
+
+    aiDecision: {
+      type: String,
+      enum: ["REMOVE_CONTENT", "BAN_USER", "IGNORE", "UNCERTAIN"],
+      default: null,
+    },
+    aiConfidence: { type: Number, min: 0, max: 1, default: null },
+    aiReason: { type: String, default: null },
+
+    status: {
+      type: String,
+      enum: ["PENDING", "REVIEWING", "RESOLVED", "DISMISSED"],
+      default: "PENDING",
+    },
+    actionTaken: {
+      type: [String],
+      enum: ["REMOVE_CONTENT", "BAN_USER", "WARN_USER", "NO_ACTION"],
+      default: [],
+    },
+    adminReason: { type: String, maxlength: 150, minlength: 3, default: null },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Report", ReportSchema);
