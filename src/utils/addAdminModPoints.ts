f6922bff8a1adecab730f60ace474c139ab666d7
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2026-01-01 18:40:09 +0400

    feat(utils): add better ttl handling in luaScript

--- src/utils/addAdminModPoints.ts
+++ src/utils/addAdminModPoints.ts
@@ -19,16 +19,20 @@ async function addAdminModPoints(
   const pointsToAdd = MOD_POINTS[actionTaken];
 
   const luaScript = `
-    local current = redis.call("GET", KEYS[1])
+  local current = redis.call("GET", KEYS[1])
     if not current then
       redis.call("SET", KEYS[1], ARGV[1], "EX", ARGV[2])
       return ARGV[1]
     else
       local newVal = tonumber(current) + tonumber(ARGV[1])
-      redis.call("SET", KEYS[1], newVal, "EX", redis.call("TTL", KEYS[1]))
+      local ttl = redis.call("TTL", KEYS[1])
+      if ttl < 0 then
+        ttl = ARGV[2]
+      end
+      redis.call("SET", KEYS[1], newVal, "EX", ttl)
       return newVal
     end
-  `;
+`;
 
   const newPoints = await redisCacheClient.eval(
     luaScript,
