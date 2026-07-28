8b65426b5d886fa15a4f7d579a5de9e7c854643e
Kelsen23 <nikolozigoreziani@gmail.com>
2026-07-28 17:19:47 +0400

    feat: remove questionAiSuggestion queue and worker

--- src/queues/questionAiSuggestion.queue.ts
+++
@@ -1,9 +0,0 @@
-import { Queue } from "bullmq";
-
-import { redisMessagingClientConnection } from "../config/redis.config.js";
-
-const questionAiSuggestionQueue = new Queue("questionAiSuggestionQueue", {
-  connection: redisMessagingClientConnection,
-});
-
-export default questionAiSuggestionQueue;
