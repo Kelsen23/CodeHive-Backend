daa02cd1bf032e2b7d7a0847135488e555741c98
Kelsen23 <nikolozigoreziani@gmail.com>
2026-07-28 17:20:56 +0400

    feat(validations/question/shared): add objectId schema

--- src/validations/question/shared.ts
+++ src/validations/question/shared.ts
@@ -1,6 +1,12 @@
 import { createRequire } from "module";
+import z from "zod";
 
 const require = createRequire(import.meta.url);
+
 const leoProfanity = require("leo-profanity");
 
-export { leoProfanity };
+const objectIdSchema = z
+  .string()
+  .regex(/^[a-f\d]{24}$/i, "Invalid ObjectId");
+
+export { leoProfanity, objectIdSchema };
