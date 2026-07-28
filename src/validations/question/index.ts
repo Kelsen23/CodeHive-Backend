081f768816b593b8d14bd336c5be2f7079006d87
Kelsen23 <nikolozigoreziani@gmail.com>
2026-07-28 17:25:20 +0400

    feat(validations/question/index): add export for suggestion schemas

--- src/validations/question/index.ts
+++ src/validations/question/index.ts
@@ -2,3 +2,4 @@ export * from "./aiRequest.schema.js";
 export * from "./content.schema.js";
 export * from "./eligibilityGate.schema.js";
 export * from "./securityVerifier.schema.js";
+export * from "./suggestion.schema.js";
